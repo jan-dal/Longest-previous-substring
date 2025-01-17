7b204f4a1efaadc6c689b41592e10921b3cee357
jan-dal <jan.dalecki@protonmail.com>
2025-01-17 23:01:55 +0100

    Add lpf results for fib words

--- src/constants.h
+++ src/constants.h
@@ -9,6 +9,11 @@
 #define ADDITIONAL_PADDING 3
 #define DEBUG 0
 
+#define CSV_HEADER_LEN 6
+#define TIME_BENCHMARK_HEADER "IN_LEN,STR_TYPE,TRIES,ALPHABET_SIZE,ALG,NALG"
+#define SA_BENCH_FILENAME "results/sa_bench_results_fib.csv"
+#define LPF_BENCH_FILENAME "results/lpf_bench_results.csv"
+
 #if DEBUG
     #define LOG_MESSAGE(...) printf(__VA_ARGS__)
     #define LOG_FUNC(func, ...) func(__VA_ARGS__)
@@ -16,6 +21,21 @@
     #define LOG_MESSAGE(...)
     #define LOG_FUNC(...)
 #endif
+
+#define SEC_TO_NANO(sec) (long long)(1e9 * sec)
+
+enum Algorithm {
+    SUFFIX_ARRAY,
+    LCP,
+    LPF,
+};
+typedef enum Algorithm Algorithm;
+
+enum StrType {
+    RANDOM,
+    FIBONACCI,
+};
+typedef enum StrType StrType;
 
 struct tuple_info {
     int *positions;
@@ -26,5 +46,14 @@ struct tuple_info {
     int max_name;
 };
 typedef struct tuple_info tuple_info;
+
+struct data_frame {
+    char *header;
+    char *filename;
+    long long **data;
+    int datapoints;
+    int header_len;
+};
+typedef struct data_frame data_frame;
 
 #endif
